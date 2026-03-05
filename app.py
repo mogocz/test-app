12c9abafbc83fb922d7a532a671ab7e62a4b0e40
Sedlo <mogocz@gmail.com>
2026-03-05 16:28:01 +0100

    #

--- app.py
+++ app.py
@@ -20,3 +20,20 @@ if uploaded is not None:
         f.write(uploaded.read())
 
 st.markdown("[A025552378N0204](https://drive.google.com/file/d/1YmUETQCNLsxvRr1rdYal_GuBGaz6qvcZ/view?usp=sharing)")
+
+
+import re
+import requests
+
+file_id = "1YmUETQCNLsxvRr1rdYal_GuBGaz6qvcZ"
+url = "https://drive.google.com/uc?export=download"
+
+r = requests.get(url, params={"id": file_id}, stream=True, allow_redirects=True)
+
+cd = r.headers.get("Content-Disposition", "")
+m = re.search(r'filename\*?=(?:UTF-8\'\')?"?([^";]+)"?', cd)
+if m:
+    print("Filename:", m.group(1))
+else:
+    print("Filename not found in headers (maybe needs confirmation token or is not public).")
+
