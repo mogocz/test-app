a78b9f7eb7002e95df34d03f265a238b813b93b0
Sedlo <mogocz@gmail.com>
2026-03-05 15:39:42 +0100

    test

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 
 import streamlit as st
 
+st.title("This is a title")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.title("Moje první webová apka")
 
 name = st.text_input("Jak se jmenuješ?")
 if st.button("pozdrav Mě hned"):
     st.write(f"Ahoj, {name} 👋")
 
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
