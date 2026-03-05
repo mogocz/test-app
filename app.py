3ec15cf5c49c3920f20430cc5bff9f3531cf868c
Sedlo <mogocz@gmail.com>
2026-03-05 15:59:05 +0100

    #

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ if st.button("pozdrav Mě hned"):
 import streamlit as st
 
 uploaded = st.file_uploader("Nahraj soubor", type=["txt", "csv"])
+
 if uploaded is not None:
-    data = uploaded.read()   # bytes [8](https://deepwiki.com/streamlit/streamlit/10-file-handling-and-uploads)
-    st.write("Velikost:", len(data), "B")
+    with open(uploaded.name, "wb") as f:
+        f.write(uploaded.read())
