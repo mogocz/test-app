41d6bf7653eeeaa384768ec0ec021e1f8d9ad5db
Sedlo <mogocz@gmail.com>
2026-03-05 16:00:08 +0100

    #

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.title("Moje první webová apka")
 
 name = st.text_input("Jak se jmenuješ?")
-if st.button("pozdrav Mě hned"):
+if st.button("pozdrav Mě"):
     st.write(f"Ahoj, {name} 👋")
 
 
