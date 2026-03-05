d6ff28adc1a6c0e5aab7642698bfa02a96481f0d
Sedlo <mogocz@gmail.com>
2026-03-05 15:23:59 +0100

    poslední komentář

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import streamlit as st
 st.title("Moje první webová apka")
 
 name = st.text_input("Jak se jmenuješ?")
-if st.button("pozdrav Mě chacha"):
+if st.button("pozdrav Mě"):
     st.write(f"Ahoj, {name} 👋")
