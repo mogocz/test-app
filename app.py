31fb6c772e105de8f0db283620614bfceac03d5a
Sedlo <mogocz@gmail.com>
2026-03-05 15:48:39 +0100

    #

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ name = st.text_input("Jak se jmenuješ?")
 if st.button("pozdrav Mě hned"):
     st.write(f"Ahoj, {name} 👋")
 
+
+from pathlib import Path
+
+Path("data").mkdir(exist_ok=True)
+Path("data/pozdrav.txt").write_text("Ahoj!", encoding="utf-8")
+text = Path("data/pozdrav.txt").read_text(encoding="utf-8")
+print(text)
+
+
